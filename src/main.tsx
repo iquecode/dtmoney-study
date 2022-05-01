5299a59f6832b6bcca0171129967391a35dfabb0
ique <ique@pop-os.localdomain>
2022-05-01 07:31:32 -0300

    modal new transaction - style ok

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { GlobalStyle } from "./style/global";
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', ()=> {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
